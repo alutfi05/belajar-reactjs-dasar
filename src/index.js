07b34939b799a50bc6f2f1e446b2cb24453ac7ab
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-05-25 15:26:04 +0700

    learning react fragment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const Greeting = () => {
-    return <h2>My First Component</h2>;
+    return (
+        <>
+            <div>
+                <h2>hello people</h2>
+                <ul>
+                    <li>
+                        <a href="#">hello world</a>
+                    </li>
+                </ul>
+            </div>
+            <h2>hello world</h2>
+        </>
+    );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<Greeting />);
